a5d99b5e28b6d21086aaa97224e9790cc1f1a578
Hong Sung <hsung@uco.edu>
2021-09-07 09:35:15 -0500

    super.paintComponent

--- MyCanvas.java
+++ MyCanvas.java
@@ -13,20 +13,20 @@ public class MyCanvas extends JPanel {
 	private ArrayList<Shape> things2Draw;
 
 	public MyCanvas() {
-		setPreferredSize(new Dimension(500, 300));
-
+		setPreferredSize(new Dimension(500, 400));
 		things2Draw = new ArrayList<>();
-		things2Draw.add(new Rectangle2D.Double(10, 10 , 50, 50));
-		things2Draw.add(new Rectangle2D.Double(20, 20 , 70, 50));
+		things2Draw.add(new Rectangle2D.Double(200, 180 , 50, 50));
+		things2Draw.add(new Rectangle2D.Double(220, 240 , 70, 50));
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 
 		for (Shape s: things2Draw) {
 			g2.setColor(Color.RED);
-			g2.draw(s);
+			g2.fill(s);
 		}
 	}
 
